3c8a8e07d5d9735447f3ce77823873e2fe6a2ce6
bendp <bendpetch@gmail.com>
2024-08-25 11:25:08 +0100

    Reverted IMEX O1 test

--- Tests/IMEX_O1_01.cpp
+++ Tests/IMEX_O1_01.cpp
@@ -74,8 +74,8 @@ TEST_F(IMEXO1, SplitKEEnergyMatch)
             << "Testing for enthalpy_method = reuse_pressure..." << std::endl
             << std::endl;
 
-    compute_flux_imex_o1(0, geom, statein, statein, m_pressure, fluxes,
-                         MultiFab(), iMultiFab(), dt, bc_data, settings);
+    compute_flux_imex_o1(0, geom, statein, statein, m_pressure, fluxes, dt,
+                         bc_data, settings);
 
     ASSERT_FALSE(fluxes[0].contains_nan());
     ASSERT_FALSE(fluxes[1].contains_nan());
@@ -217,8 +217,8 @@ TEST_F(IMEXO1, ExplicitKEEnergyMatch)
             << "Testing for enthalpy_method = reuse_pressure..." << std::endl
             << std::endl;
 
-    compute_flux_imex_o1(0, geom, statein, statein, m_pressure, fluxes,
-                         MultiFab(), iMultiFab(), dt, bc_data, settings);
+    compute_flux_imex_o1(0, geom, statein, statein, m_pressure, fluxes, dt,
+                         bc_data, settings);
 
     ASSERT_FALSE(fluxes[0].contains_nan());
     ASSERT_FALSE(fluxes[1].contains_nan());
@@ -272,8 +272,8 @@ TEST_F(IMEXO1, GridSplitting)
 
     // compute first without grid splitting
     setup(false);
-    compute_flux_imex_o1(0, geom, statein, statein, m_pressure, fluxes,
-                         MultiFab(), iMultiFab(), dt, bc_data, settings);
+    compute_flux_imex_o1(0, geom, statein, statein, m_pressure, fluxes, dt,
+                         bc_data, settings);
 
     Array<MultiFab, AMREX_SPACEDIM> nonsplit_fluxes;
     for (int d = 0; d < AMREX_SPACEDIM; ++d)
@@ -286,8 +286,8 @@ TEST_F(IMEXO1, GridSplitting)
     TearDown();
     // compute with grid splitting
     setup(true);
-    compute_flux_imex_o1(0, geom, statein, statein, m_pressure, fluxes,
-                         MultiFab(), iMultiFab(), dt, bc_data, settings);
+    compute_flux_imex_o1(0, geom, statein, statein, m_pressure, fluxes, dt,
+                         bc_data, settings);
 
     // compare solutions
     MFIter::allowMultipleMFIters(true);
