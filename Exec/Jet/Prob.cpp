e3d17e485484433d2467600186d283f41516f71a
bendp <bendpetch@gmail.com>
2024-09-12 13:00:37 +0100

    Updated jet inputs

--- Exec/Jet/Prob.cpp
+++ Exec/Jet/Prob.cpp
@@ -26,7 +26,7 @@ void initdata(MultiFab &S_tmp, [[maybe_unused]] const Geometry &geom)
     const Real eps  = AmrLevelAdv::h_parm->epsilon;
 
     Real exit_v_ratio, primary_exit_M, p_amb, T_amb, T_p, T_s, R_spec,
-        inner_radius, outer_radius, scale_factor;
+        inner_radius, outer_radius, wall_thickness, scale_factor;
     ParmParse ppinit("init");
     ppinit.get("exit_v_ratio", exit_v_ratio);
     ppinit.get("primary_exit_M", primary_exit_M);
@@ -41,22 +41,39 @@ void initdata(MultiFab &S_tmp, [[maybe_unused]] const Geometry &geom)
     ParmParse ppls("ls");
     ppls.get("inner_radius", inner_radius);
     ppls.get("outer_radius", outer_radius);
+    ppls.get("wall_thickness", wall_thickness);
     // ppls.get("nozzle_center", center);
     ppls.get("scale_factor", scale_factor);
     inner_radius *= scale_factor;
     outer_radius *= scale_factor;
+    wall_thickness *= scale_factor;
     // center doesn't get scaled
 
     const Real dens_amb = p_amb / (R_spec * T_amb);
-    // const Real c        = sqrt(p_amb * adia / dens_amb);
-    // const Real v_p      = primary_exit_M * c;
-    // const Real v_s      = exit_v_ratio * v_p;
+    const Real c        = sqrt(p_amb * adia / dens_amb);
+    const Real v_p      = primary_exit_M * c;
+    const Real v_s      = exit_v_ratio * v_p;
 
     const GpuArray<Real, EULER_NCOMP> init_primv{ dens_amb,
                                                   AMREX_D_DECL(0, 0, 0),
                                                   p_amb };
 
-    const auto &init_consv = consv_from_primv(init_primv, adia, eps);
+    // Guess for initial conditions of jet
+    const Real                        p_p = dens_amb * T_p * R_spec;
+    const Real                        p_s = dens_amb * T_s * R_spec;
+    const GpuArray<Real, EULER_NCOMP> init_p_primv{ dens_amb,
+                                                    AMREX_D_DECL(v_p, 0, 0),
+                                                    p_p };
+    const GpuArray<Real, EULER_NCOMP> init_s_primv{ dens_amb,
+                                                    AMREX_D_DECL(v_s, 0, 0),
+                                                    p_s };
+
+    const auto &init_consv   = consv_from_primv(init_primv, adia, eps);
+    const auto &init_p_consv = consv_from_primv(init_p_primv, adia, eps);
+    const auto &init_s_consv = consv_from_primv(init_s_primv, adia, eps);
+
+    const auto &dx      = geom.CellSizeArray();
+    const auto &prob_lo = geom.ProbLoArray();
 
 #ifdef AMREX_USE_OMP
 #pragma omp parallel if (Gpu::notInLaunchRegion())
@@ -69,7 +86,17 @@ void initdata(MultiFab &S_tmp, [[maybe_unused]] const Geometry &geom)
 
             ParallelFor(bx, EULER_NCOMP,
                         [=] AMREX_GPU_DEVICE(int i, int j, int k, int n)
-                        { arr(i, j, k, n) = init_consv[n]; });
+                        {
+                            const Real r = (j + 0.5) * dx[1] + prob_lo[1];
+                            if (r < inner_radius - 0.5 * wall_thickness)
+                                arr(i, j, k, n) = init_p_consv[n];
+                            else if (inner_radius + 0.5 * wall_thickness < r
+                                     && r < outer_radius
+                                                - 0.5 * wall_thickness)
+                                arr(i, j, k, n) = init_s_consv[n];
+                            else
+                                arr(i, j, k, n) = init_consv[n];
+                        });
         }
     }
 }
